2e6c0cb1cbffd6e6f8fd471f00b340c772c76b1b
Tau <tau@tau.zone>
2018-03-24 20:29:31 +0000

    Initial commit

---
+++ site.pp
@@ -0,0 +1,16 @@
+# $Tau: puppet/manifests/site.pp $
+
+## ------------------------------------------------------------------------- ##
+##                          T A U    P R O J E C T                           ##
+## ------------------------------------------------------------------------- ##
+##                                                                           ##
+## This file forms part of the Tau Project and is subject to copyright.      ##
+##                                                                           ##
+## For full licensing terms, including conditions for redistribution, please ##
+## consult the README document accompanying the source distribution:         ##
+##                                                                           ##
+##   <https://github.com/tauproject/puppet>                                  ##
+##                                                                           ##
+## ------------------------------------------------------------------------- ##
+
+include tau
